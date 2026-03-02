a7ca2e7c686882410c1ea2a1c8ce78d8c9b0b2e7
nixpham <nixpham0912@gmail.com>
2026-03-02 16:00:50 -0500

    camera updates

--- Assets/Scripts/Dialogue/NPC.cs
+++ Assets/Scripts/Dialogue/NPC.cs
@@ -16,9 +16,6 @@ public class NPC : MonoBehaviour
     public GameObject dialogueRoot;
     public TMP_Text dialogueText;
 
-    [Header("Dialogue Buttons")]
-    public DialogueButtons dialogueButtons;
-
     [Header("Name Frames")]
     public GameObject leftNameFrame;
     public TMP_Text leftNameText;
